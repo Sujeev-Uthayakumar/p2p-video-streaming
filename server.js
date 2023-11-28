89218b5c3e2436fceaa85a7f763077aea0a17e44
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2023-11-28 09:26:39 -0500

    add frontend

--- server.js
+++ server.js
@@ -1,27 +1,61 @@
 const express = require("express");
+const http = require("http");
 const app = express();
-const server = require("http").Server(app);
-const io = require("socket.io")(server);
-const { v4: uuidv4 } = require("uuid");
-
-const PORT = process.env.PORT || 3000;
-
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  res.redirect(`/${uuidv4()}`);
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
-app.get("/:room", (req, res) => {
-  res.render("room", { roomId: req.params.room });
+const PORT = process.env.PORT || 5000;
+
+// Configure multer for video file storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + "." + file.mimetype.split("/")[1]
+    );
+  },
+});
+
+const upload = multer({ storage: storage });
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Routes
+app.post("/upload", upload.single("video"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send("No file uploaded.");
+  }
+  return res.send("Video Uploaded Successfully!");
 });
 
 io.on("connection", (socket) => {
-  socket.on("join-room", (roomId, userId) => {
-    socket.join(roomId);
-    socket.to(roomId).broadcast.emit("user-connected", userId);
+  socket.emit("me", socket.id);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("stream-ended");
+  });
+
+  socket.on("join-room", (data) => {
+    io.to(data.userToCall).emit("callUser", {
+      signal: data.signalData,
+      from: data.from,
+      name: data.name,
+    });
+  });
+
+  socket.on("", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
   });
 });
 
-server.listen(PORT);
+server.listen(PORT, () => console.log("server is running on port 5000"));
